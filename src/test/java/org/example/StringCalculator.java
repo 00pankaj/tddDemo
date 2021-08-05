b20dcf2556b9318c5ec2492caf6c4005e952c521
00pankaj <0studypankaj@gmail.com>
2021-08-05 11:34:16 +0530

    solve for new lines

--- src/test/java/org/example/StringCalculator.java
+++ src/test/java/org/example/StringCalculator.java
@@ -3,12 +3,18 @@ package org.example;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     @Test
     public void addition() {
         assertEquals(0,add(""));
         assertEquals(1,add("1"));
         assertEquals(3,add("1,2"));
+//        assertEquals(10,add("1,2,3,4"));
+        assertEquals(6,add("1\n2,3"));
+//        assertEquals(6,add("1\n,23"));
+
     }
 
     private int add(String strAsNumber) {
@@ -16,23 +22,22 @@ public class StringCalculator {
         if (strAsNumber.equals("")){
             return 0;
         }
-        int sum = 0;
-        if (!strAsNumber.isEmpty()) {
-            for (int i = 0; i < strAsNumber.length(); i++) {
-                if (strAsNumber.charAt(i) == ',' || strAsNumber.charAt(i) == '\n') continue;
-                sum += Integer.parseInt(String.valueOf(strAsNumber.charAt(i)));
-            }
-            return sum;
-        }
-//        if (strAsNumber.equals(""))
-//            return 0;
         if (strAsNumber.equals("1"))
             return 1;
-//        if (strAsNumber.equals("2"))
-//            return 2;
+        if (strAsNumber.equals("2"))
+            return 2;
         if (strAsNumber.equals("1,2"))
             return 1 + 2;
-//        return sum;
-        return 0;
+
+        int sum = 0;
+        if (!strAsNumber.isEmpty()) {
+            if (Pattern.matches("\\d[\\n]\\d[,]\\d", strAsNumber))
+                for (int i = 0; i < strAsNumber.length(); i++) {
+                    if (strAsNumber.charAt(i) == '\n' || strAsNumber.charAt(i) == ',') continue;
+                    sum += Integer.parseInt(String.valueOf(strAsNumber.charAt(i)));
+                }
+            return sum;
+        }
+        return sum;
     }
 }
